e79a3f95f8ba22eb56e927c3ab54379c6a5b5ac4
eraldss1 <erald.muhammad@gmail.com>
2020-12-02 03:44:35 +0700

    fix: fix import

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route_annotations.dart';
-import 'package:bintex_mobile_driver/ui/views/auth/auth_view.dart';
-import 'package:bintex_mobile_driver/ui/views/home/home_view.dart';
+
+import '../ui/views/auth/auth_view.dart';
+import '../ui/views/home/home_view.dart';
 
 @MaterialAutoRouter(
   routes: <AutoRoute>[
