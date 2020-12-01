07e05550873bbd4cc767335d7df6f23f7164989b
eraldss1 <erald.muhammad@gmail.com>
2020-12-02 03:43:50 +0700

    style: add value of colors

---
+++ lib/ui/values.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class BintexColor {
+  static Color primary100(double opacity) {
+    return Color.fromRGBO(42, 61, 102, opacity);
+  }
+
+  static Color primary200() {
+    return const Color(0xFFF4F7FF);
+  }
+
+  static Color primary300() {
+    return const Color(0xFFFFFFFF);
+  }
+
+  static Color accent100() {
+    return const Color(0xFF5D54A4);
+  }
+
+  static Color accent200() {
+    return const Color(0xFFB99AFF);
+  }
+
+  static Color accent300() {
+    return const Color(0xFFD8C7FF);
+  }
+}
