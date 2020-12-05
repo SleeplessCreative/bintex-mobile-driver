e55bb11da85b193baa42318fec2db95e2ed1347c
eraldss1 <erald.muhammad@gmail.com>
2020-12-05 22:38:17 +0700

    perf: routing

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,12 +1,15 @@
 import 'package:auto_route/auto_route_annotations.dart';
+import 'package:bintex_mobile_driver/ui/views/add/add_view.dart';
 
 import '../ui/views/auth/auth_view.dart';
 import '../ui/views/home/home_view.dart';
+import '../ui/views/add/add_view.dart';
 
 @MaterialAutoRouter(
   routes: <AutoRoute>[
     MaterialRoute(page: HomeView),
     MaterialRoute(page: AuthView, initial: true),
+    MaterialRoute(page: AddView),
   ],
 )
 class $Router {}
